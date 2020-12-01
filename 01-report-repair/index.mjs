7df138b500b2e8e52a7a3e104fd29173af3f7c49
John Sanderson <john.sanderson@bbc.co.uk>
2020-12-01 20:34:10 +0000

    Day 1 refactored

--- 01-report-repair/index.mjs
+++ 01-report-repair/index.mjs
@@ -1,29 +1,42 @@
 import { readFile } from 'fs/promises';
 
+const BREAK = Symbol('break');
+
 const sumSet = (set) => set.reduce((sum, val) => sum + val, 0);
 const timesSet = (set) => set.reduce((prod, val) => prod * val, 1);
 const doesSetMatch = (sum, sumToFind) => sum === sumToFind;
 const isMatchPossible = (sum, sumToFind) => sum <= sumToFind;
+const breakReducer = (fn) => (acc, val) => acc || (acc === BREAK ? BREAK : fn(val));
+
+const doMatch = (results, sumToFind) => {
+    const sum = sumSet(results);
+    return doesSetMatch(sum, sumToFind)
+        ? results
+        : isMatchPossible(sum, sumToFind)
+            ? false
+            : BREAK;
+}
 
 const findSumFromLists = (lists, sumToFind, results = []) => {
     if (!lists.length) {
-        const sum = sumSet(results);
-        return doesSetMatch(sum, sumToFind)
-            ? results
-            : isMatchPossible(sum, sumToFind)
-                ? false
-                : -1;
+        return doMatch(results, sumToFind);
     }
 
-    const remainder = lists.slice(1) || [];
-    const result = lists[0].reduce(((acc, val) =>
-        acc || (acc === -1 ? -1 : findSumFromLists(remainder, sumToFind, [...results, val]))
-    ), false);
+    const result = lists[0].reduce(
+        breakReducer((val) =>
+            findSumFromLists(
+                lists.slice(1) || [],
+                sumToFind,
+                [...results, val]
+            )
+        ),
+        false
+    );
 
-    return result === -1 ? false : result;
+    return result === BREAK ? false : result;
 };
 
-const findSum = (inputList, numberOfItems = 2, sumToFind = 2020) => {
+export const findSum = (inputList, numberOfItems, sumToFind = 2020) => {
     const sortedList = inputList.sort((a, b) => a >= b ? 1 : -1);
     const slicedList = sortedList.slice(0, sortedList.find((val) => val >= (sumToFind - sortedList[0])));
     const reversedList = [...slicedList].reverse();
@@ -39,7 +52,7 @@ const findSum = (inputList, numberOfItems = 2, sumToFind = 2020) => {
     return findSumFromLists(lists, sumToFind);
 };
 
-const findSumMultiple = async (numberOfItems, inputPath) => {
+export const findSumMultiple = async (numberOfItems, inputPath) => {
     const input = await readFile(inputPath, 'utf8');
     const list = input.split('\n').map(Number);
     return timesSet(findSum(list, numberOfItems));
@@ -50,4 +63,5 @@ export const main = async (inputPath = './input.txt') => {
     console.log(`Part Two: ${await findSumMultiple(3, inputPath)}`);
 }
 
+console.log(require);
 main();

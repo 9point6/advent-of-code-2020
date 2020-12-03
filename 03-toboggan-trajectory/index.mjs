21e5987311fe6b1a76afc84a94c8ce8709b4365b
John Sanderson <john.sanderson@bbc.co.uk>
2020-12-03 09:24:08 +0000

    Day 3 part 2

--- 03-toboggan-trajectory/index.mjs
+++ 03-toboggan-trajectory/index.mjs
@@ -1,12 +1,21 @@
 import { readFile } from 'fs/promises';
 
+const SLOPES = [
+    { x: 1, y: 1 },
+    { x: 3, y: 1 },
+    { x: 5, y: 1 },
+    { x: 7, y: 1 },
+    { x: 1, y: 2 }
+];
+
 const getCoordinate = (map, { x, y }) => map[y][x % map[0].length]
 
 const threeRightOneDown = ({ x, y }) => ({ x: x + 3, y: y + 1 });
+const changeCoordinates = ({ x, y }, change) => ({ x: x + change.x, y: y + change.y });
 
 const parseMap = (file) => file.split('\n').map((line) => line.split(''));
 
-export const treesHit = async (inputPath) => {
+export const treesHit = async (inputPath, slope) => {
     const map = parseMap(await readFile(inputPath, 'utf8'));
 
     let current = { x: 0, y: 0 }
@@ -17,14 +26,22 @@ export const treesHit = async (inputPath) => {
             count++;
         }
 
-        current = threeRightOneDown(current);
+        current = changeCoordinates(current, slope);
     }
 
     return count;
 }
 
 export const main = async (inputPath = './input.txt') => {
-    console.log(`Hit trees: ${await treesHit(inputPath)}`);
+    const product = (await Promise.all(SLOPES
+        .map(async (slope) => {
+            const result = await treesHit(inputPath, slope);
+            console.log(`Hit trees (+${slope.x}, +${slope.y}): ${result}`);
+            return result
+        })))
+        .reduce((acc, val) => acc * val, 1);
+
+    console.log('Product:', product);
 }
 
 main();

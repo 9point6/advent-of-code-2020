a3dd08df1902cf5189e41f6c2ae9143ceca079f9
John Sanderson <john.sanderson@bbc.co.uk>
2020-12-04 11:33:26 +0000

    Day 4 part 2

--- 04-passport-processing/index.mjs
+++ 04-passport-processing/index.mjs
@@ -1,15 +1,21 @@
 import { readFile } from 'fs/promises';
 
-const REQUIRED_FIELDS = [
-    'byr',
-    'iyr',
-    'eyr',
-    'hgt',
-    'hcl',
-    'ecl',
-    'pid',
-    // 'cid',
-]
+const VALID_EYE_COLOURS = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+const REQUIRED_FIELDS = {
+    byr: (val) => !!val && val.length === 4 && 1920 <= val && val <= 2002,
+    iyr: (val) => !!val && val.length === 4 && 2010 <= val && val <= 2020,
+    eyr: (val) => !!val && val.length === 4 && 2020 <= val && val <= 2030,
+    hgt: (val = '') => {
+        const suffix = val.substr(-2, 2);
+        const value = parseInt(val, 10);
+        return (suffix === 'cm' && (150 <= value && value <= 193))
+            || (suffix === 'in' && (59 <= value && value <= 76))
+    },
+    hcl: (val) => /^#[a-f0-9]{6}$/.test(val),
+    ecl: (val) => VALID_EYE_COLOURS.includes(val),
+    pid: (val) => /^[0-9]{9}$/.test(val)
+    // cid: (val) => true,
+}
 
 const parsePassport = (passportString) => passportString
     .replace(/\n/g, ' ')
@@ -23,7 +29,9 @@ const parsePassports = (file) => file.split('\n\n').map(parsePassport);
 const validatePassports = (passports) => passports
     .map((passport) => ({
         ...passport,
-        valid: REQUIRED_FIELDS.reduce((acc, field) => acc && !!passport[field], true)
+        valid: Object.entries(REQUIRED_FIELDS)
+            .reduce((acc, [field, validator]) =>
+                acc && validator(passport[field]), true)
     }));
 
 const countValid = (validatedPassports) => validatedPassports

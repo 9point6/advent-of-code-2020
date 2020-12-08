f5e0bd1433dd948c921d5ea5b54f25fdc2ed5a18
John Sanderson <john.sanderson@bbc.co.uk>
2020-12-08 10:03:27 +0000

    Day 8 Part 2

--- 08-handheld-halting/index.mjs
+++ 08-handheld-halting/index.mjs
@@ -1,43 +1,63 @@
 import { readFile } from 'fs/promises';
 import esMain from '../00-helpers/es-main.mjs';
 
-const applyInstruction = (instruction, param, state) => {
-    const newState = { ...state };
-    switch (instruction) {
-        case 'acc':
-            newState.accumulator += Number(param);
-        case 'nop':
-            newState.pointer += 1;
-            break;
-        case 'jmp':
-            newState.pointer += Number(param);
-    }
+const parseBootloader = (file) => file.split('\n')
+    .map((line) => line.split(' '))
+    .map(([instruction, param]) => [instruction, Number(param)]);
 
-    return newState;
-}
+const applyInstruction = (instruction, param, { halt, accumulator, pointer }) => ({
+    halt,
+    accumulator: accumulator + (instruction === 'acc' ? param : 0),
+    pointer: pointer + (instruction === 'jmp' ? param : 1)
+});
 
-const parseBootloader = (file) => {
-    const instructions = file.split('\n');
+const executeBootloader = (instructions) => {
     const visited = {};
     let state = {
         pointer: 0,
-        accumulator: 0
+        accumulator: 0,
+        halt: false
     };
 
     while (true) {
-        visited[state.pointer] = true;
-        const [instruction, param] = instructions[state.pointer].split(' ');
-        state = applyInstruction(instruction, param, state)
-
-        if (visited[state.pointer]) {
-            return state.accumulator;
+        state.halt = !!visited[state.pointer];
+        if (state.halt || state.pointer >= instructions.length) {
+            break;
         }
+
+        visited[state.pointer] = true;
+        const [instruction, param] = instructions[state.pointer];
+        state = applyInstruction(instruction, param, state);
     }
+
+    return state;
+}
+
+const fixCorruptedBootloader = (instructions) => {
+    const SWAPSIES = { nop: 'jmp', jmp: 'nop' };
+
+    return instructions.reduce((state, [instruction, param], i) => {
+        if (state || !SWAPSIES[instruction]) {
+            return state;
+        }
+
+        const { halt, accumulator } = executeBootloader([
+            ...instructions.slice(0, i),
+            [SWAPSIES[instruction], param],
+            ...instructions.slice(i + 1)
+        ]);
+
+        return halt ? false : accumulator;
+    }, false)
 }
 
 export const main = async (inputPath = './input.txt') => {
-    const bootloaderResult = parseBootloader(await readFile(inputPath, 'utf8'));
-    console.log('Bootloader Result (part 1):', bootloaderResult);
+    const instructions = parseBootloader(await readFile(inputPath, 'utf8'));
+    const bootloaderResult = executeBootloader(instructions);
+    console.log('Bootloader Result (part 1):', bootloaderResult.accumulator);
+
+    const fixedBootloaderResult = fixCorruptedBootloader(instructions);
+    console.log('Fixed Bootloader Result (part 2):', fixedBootloaderResult);
 }
 
 if (esMain(import.meta)) {

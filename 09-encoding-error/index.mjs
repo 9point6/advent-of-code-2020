3577bfe9e4b17d4104218d1c563787d47ab297ed
John Sanderson <john.sanderson@bbc.co.uk>
2020-12-09 10:04:35 +0000

    Day 9 part 2

--- 09-encoding-error/index.mjs
+++ 09-encoding-error/index.mjs
@@ -3,6 +3,8 @@ import esMain from '../00-helpers/es-main.mjs';
 
 const parseCipher = (file) => file.split('\n').map(Number);
 
+const sumSet = (set) => set.reduce((acc, val) => acc + val, 0);
+
 const containsSum = (set, sum) =>
     set.reduce(
         (acc, value) => acc || set.reduce(
@@ -20,17 +22,64 @@ const validateCipher = (cipher, preambleLength = 25) => cipher
 
         return {
             value,
-            valid: containsSum(cipher.slice(i - 25, i), value)
+            valid: containsSum(cipher.slice(i - preambleLength, i), value)
         };
     })
+
+const findContiguousSum = (set, sum) => set
+    .reduce((acc, value) => {
+        if (acc.found) {
+            return acc;
+        }
+
+        const newSet = [...acc.set, value];
+        const currentSum = sumSet(newSet);
+        if (currentSum === sum) {
+            return { set: newSet, found: true }
+        }
+
+        if (currentSum > sum) {
+
+            const shortSet = newSet.slice(1);
+            const shortSum = sumSet(shortSet);
+
+            return newSet.reduce((acc, item, i) => {
+                if (acc.found) {
+                    return acc;
+                }
+
+                const shortSet = acc.set.slice(1);
+                const shortSum = sumSet(shortSet);
+                if (shortSum < sum) {
+                    return acc;
+                }
+
+                return {
+                    set: shortSet,
+                    found: shortSum === sum
+                };
+            }, { set: newSet });
+        }
+
+        return { set: newSet };
+    }, { set: [] });
 
 const findInvalid = (validatedCipher) => validatedCipher
     .find((value) => !value.preamble && !value.valid);
 
+const findWeakness = (contiguousSum) => {
+    const sortedList = contiguousSum.set.sort((a, b) => a - b);
+    return sortedList[0] + sortedList[sortedList.length - 1];
+}
+
 export const main = async (inputPath = './input.txt') => {
     const parsedCipher = parseCipher(await readFile(inputPath, 'utf8'));
     const validatedCipher = validateCipher(parsedCipher);
-    console.log('Invalid Value:', findInvalid(validatedCipher).value);
+    const invalidValue = findInvalid(validatedCipher).value;
+    console.log('Invalid Value:', invalidValue);
+    const contiguousSum = findContiguousSum(parsedCipher, invalidValue);
+    console.log('Contiguous Sum:', contiguousSum);
+    console.log('Encryption Weakness:', findWeakness(contiguousSum));
 }
 
 if (esMain(import.meta)) {

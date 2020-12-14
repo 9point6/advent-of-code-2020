ff80e83456004ec5c3049c7b690107edf9e661e3
John Sanderson <js@9point6.com>
2020-12-14 22:12:47 +0000

    Day 12 part 1 refactor, should do part too, but cyberpunk 🤷‍♂️

--- 12-rain-risk/index.mjs
+++ 12-rain-risk/index.mjs
@@ -2,7 +2,7 @@ import { readFile } from 'fs/promises';
 import esMain from '../00-helpers/es-main.mjs';
 
 const INITIAL_DIRECTION = 'E';
-const COMMAND_MAP = { N: 'NS', E: 'EW', S: 'NS', W: 'EW', L: 'LR', R: 'LR' };
+const COMMAND_MAP = { N: 'NS', E: 'WE', S: 'NS', W: 'WE', L: 'LR', R: 'LR' };
 const CLOCKWISE_ROTATION = ['N', 'E', 'S', 'W'];
 
 const parseDirection = (direction) => ({
@@ -11,39 +11,44 @@ const parseDirection = (direction) => ({
 });
 
 const parseDirections = (file) => file.split('\n').filter((line) => line).map(parseDirection);
+const simplifyCommand = ({ command, param }) => ({
+    command: COMMAND_MAP[command] || command,
+    param: ['S', 'E', 'L'].includes(command) ? -param : param
+});
+
+const absoluteDirection = (pair, facing, command, param) => 
+    (command === pair || (command === 'F' && facing === pair[0])
+        ? param : (command === 'F' && facing === pair[1]) ? -param : 0);
 
 const computeManhattan = (directions) => {
-    const { NS, EW } = directions
-        .map(({ command, param }) => ({
-            command: COMMAND_MAP[command] || command,
-            param: ['S', 'E', 'L'].includes(command) ? -param : param
-        }))
-        .reduce(({ NS, EW, facing }, { command, param }) => ({
-            NS: NS + (command === 'NS' || (command === 'F' && facing === 'N')
-                ? param
-                : (command === 'F' && facing === 'S') ? -param : 0),
-            EW: EW + (command === 'EW' || (command === 'F' && facing === 'W')
-                ? param
-                : (command === 'F' && facing === 'E') ? -param : 0),
+    const { NS, WE } = directions
+        .map(simplifyCommand)
+        .reduce(({ NS, WE, facing }, { command, param }) => ({
+            NS: NS + absoluteDirection('NS', facing, command, param),
+            WE: WE + absoluteDirection('WE', facing, command, param),
             facing: command === 'LR'
                 ? CLOCKWISE_ROTATION[
-                    (CLOCKWISE_ROTATION.length + CLOCKWISE_ROTATION.indexOf(facing) + (param / 90))
-                        % CLOCKWISE_ROTATION.length]
+                    (
+                        CLOCKWISE_ROTATION.length 
+                        + CLOCKWISE_ROTATION.indexOf(facing) 
+                        + (param / 90)
+                    ) % CLOCKWISE_ROTATION.length
+                ]
                 : facing
         }), {
             NS: 0,
-            EW: 0,
+            WE: 0,
             facing: INITIAL_DIRECTION
         });
 
-    return Math.abs(NS) + Math.abs(EW);
-}
+    return Math.abs(NS) + Math.abs(WE);
+};
 
 export const main = async (inputPath = './input.txt') => {
     const directions = parseDirections(await readFile(inputPath, 'utf8'));
     const manhattanDistance = computeManhattan(directions);
     console.log('manhattan distance:', manhattanDistance);
-}
+};
 
 if (esMain(import.meta)) {
     main(process.env.INPUT_PATH || './input.txt');
